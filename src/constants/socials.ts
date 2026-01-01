8a8335b016e6a124a2f4469f52ad7820dc65ce3a
Thalanas110 <amdimate43@gmail.com>
2026-01-01 14:12:24 +0800

    replaced the youtube video with a new youtube video

---
+++ src/constants/socials.ts
@@ -0,0 +1,5 @@
+// Social media and external links
+export const DISCORD_URL = 'https://discord.gg/warofdots';
+export const YOUTUBE_CHANNEL_URL = 'https://www.youtube.com/@TeaAndPython';
+export const YOUTUBE_FEATURED_VIDEO_URL = 'https://www.youtube.com/embed/Cb0aTaZtlvA';
+export const BUY_ME_COFFEE_URL = 'https://buymeacoffee.com/pythonic_tea';
