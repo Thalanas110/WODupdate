62e30cfea078577571e085482281554a7baafaa6
Thalanas110 <amdimate43@gmail.com>
2026-01-07 00:28:59 +0800

    added more and minor documentation fix

--- src/constants/socials.ts
+++ src/constants/socials.ts
@@ -1,10 +1,10 @@
-// Social media and external links
+// social media
 export const DISCORD_URL = 'https://discord.gg/warofdots';
 export const YOUTUBE_CHANNEL_URL = 'https://www.youtube.com/@TeaAndPython';
 export const YOUTUBE_FEATURED_VIDEO_URL = 'https://www.youtube.com/embed/Cb0aTaZtlvA';
 export const BUY_ME_COFFEE_URL = 'https://buymeacoffee.com/pythonic_tea';
 
+// guide urls   
 export const GUIDE_URL = 'https://docs.google.com/document/d/17dbGNQC-BPwQHfUeeI1-0o5-_Gohf6XWWTObdLhV9VA/edit?tab=t.0';
-
 export const MODDING_URL = "modding-url-here";
 export const SKIN_URL = "https://github.com/PixelG-t/wodskin";
