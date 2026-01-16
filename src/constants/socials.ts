3544cd80cdd1d173daf7ff2d127f1e1b5f26b55f
Thalanas110 <amdimate43@gmail.com>
2026-01-16 09:45:57 +0800

    added actual mapping url tutorial from tap

--- src/constants/socials.ts
+++ src/constants/socials.ts
@@ -5,6 +5,6 @@ export const YOUTUBE_FEATURED_VIDEO_URL = 'https://www.youtube.com/embed/Cb0aTaZ
 export const BUY_ME_COFFEE_URL = 'https://buymeacoffee.com/pythonic_tea';
 
 // guide urls   
-export const GUIDE_URL = 'https://docs.google.com/document/d/17dbGNQC-BPwQHfUeeI1-0o5-_Gohf6XWWTObdLhV9VA/edit?tab=t.0';
-export const MODDING_URL = "modding-url-here";
-export const SKIN_URL = "https://github.com/PixelG-t/wodskin";
+export const GUIDE_URL = 'https://docs.google.com/document/d/17dbGNQC-BPwQHfUeeI1-0o5-_Gohf6XWWTObdLhV9VA/edit?tab=t.0'; // comprehensive
+export const MODDING_URL = "https://www.youtube.com/watch?v=h6E48pvkeE4&t=89s"; //maps
+export const SKIN_URL = "https://github.com/PixelG-t/wodskin"; // skinning
