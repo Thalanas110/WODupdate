20c65164a075e6645d154b15dc018b2657655bc8
Thalanas110 <amdimate43@gmail.com>
2026-01-17 14:49:37 +0800

    replace skin url in socials.ts

--- src/constants/socials.ts
+++ src/constants/socials.ts
@@ -7,7 +7,7 @@ export const BUY_ME_COFFEE_URL = 'https://buymeacoffee.com/pythonic_tea';
 // guide urls   
 export const GUIDE_URL = 'https://docs.google.com/document/d/17dbGNQC-BPwQHfUeeI1-0o5-_Gohf6XWWTObdLhV9VA/edit?tab=t.0'; // comprehensive
 export const MODDING_URL = "https://www.youtube.com/watch?v=h6E48pvkeE4&t=89s"; //maps
-export const SKIN_URL = "https://github.com/PixelG-t/wodskin"; // skinning
+export const SKIN_URL = "https://github.com/PixelG-t/wodskin/blob/main/HOWTO.md"; // skinning
 
 // steam link
 export const STEAM_URL = 'https://store.steampowered.com/app/3902430/War_of_Dots/';
