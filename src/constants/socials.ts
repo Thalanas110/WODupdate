4bc13faec96b5ba46d73193cf2bdb920641a4af0
Thalanas110 <amdimate43@gmail.com>
2026-02-11 22:55:02 +0800

    replace video link

--- src/constants/socials.ts
+++ src/constants/socials.ts
@@ -1,7 +1,7 @@
 // social media
 export const DISCORD_URL = 'https://discord.gg/warofdots';
 export const YOUTUBE_CHANNEL_URL = 'https://www.youtube.com/@TeaAndPython';
-export const YOUTUBE_FEATURED_VIDEO_URL = 'https://www.youtube.com/embed/Cb0aTaZtlvA';
+export const YOUTUBE_FEATURED_VIDEO_URL = 'https://youtu.be/bsUL9PjzGMw?si=mJIjafh3FxYjFjoq';
 export const BUY_ME_COFFEE_URL = 'https://buymeacoffee.com/pythonic_tea';
 
 // guide urls   
